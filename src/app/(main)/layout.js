74d2e001de71dcb949c498806545c213baac29e0
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-16 12:00:29 +0600

    update: updated main layout

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+
+import Navbar from "@/components/shared/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
