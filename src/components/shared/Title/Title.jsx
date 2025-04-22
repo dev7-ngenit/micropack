522b22dff259dd0e664f586dad803172acc81584
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-22 12:34:15 +0600

    update: completed Title design

--- src/components/shared/Title/Title.jsx
+++ src/components/shared/Title/Title.jsx
@@ -1,5 +1,14 @@
 import { cn } from "@/lib/utils";
 
 export default function Title({ children, className }) {
-  return <h1 className={cn("text-2xl font-bold", className)}>{children}</h1>;
+  return (
+    <h1
+      className={cn(
+        "w-fit bg-[linear-gradient(90deg,_transparent_0,_rgba(251,_207,_0,_0.5)_15%,_rgba(251,_207,_0,_0.5)_85%,_transparent_100%)] bg-size-[100%_35%] bg-repeat-x text-3xl font-bold [background-position-y:center]",
+        className,
+      )}
+    >
+      {children}
+    </h1>
+  );
 }
