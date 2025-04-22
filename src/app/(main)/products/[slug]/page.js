3c68ac8af548b0f464214c901b9828c05c0b50c4
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-22 12:36:35 +0600

    add: created ProductDescription component and made the design mobile responsive

--- src/app/(main)/products/[slug]/page.js
+++ src/app/(main)/products/[slug]/page.js
@@ -1,9 +1,12 @@
+import ProductDescription from "@/components/products/slug/ProductDescription/ProductDescription";
 import ProductDetails from "@/components/products/slug/ProductDetails/ProductDetails";
 
 export default function page() {
   return (
     <main className="container !my-16 md:min-h-[calc(100dvh-33rem)]">
       <ProductDetails />
+
+      <ProductDescription />
     </main>
   );
 }
---
+++ src/components/products/slug/ProductDescription/ProductDescription.jsx
@@ -0,0 +1,17 @@
+import Title from "@/components/shared/Title/Title";
+
+export default function ProductDescription() {
+  return (
+    <section className="mx-3 mt-14 min-h-40 md:mx-0">
+      <Title>Description</Title>
+
+      <div className="mt-2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa
+        architecto dolorum esse ab veniam laborum impedit sint, hic voluptatum.
+        Ipsa cupiditate fuga, asperiores aspernatur exercitationem deleniti ex
+        laborum autem quaerat excepturi fugit error aliquid quis sunt iste
+        quasi, voluptate reiciendis!
+      </div>
+    </section>
+  );
+}
