3425570188dfdb504ac1f0cece32e2a2746de88a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-27 11:13:10 +0600

    update: made the footer links working

--- src/components/shared/Footer/Support.jsx
+++ src/components/shared/Footer/Support.jsx
@@ -1,32 +1,66 @@
+import Link from "next/link";
+
 export default function Support() {
   return (
     <div>
       <h4 className="mb-4 text-xl text-white">Support</h4>
 
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Contact Us
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        About Us
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Shipping Policy
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Refund Policy{" "}
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Privacy Policy
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Terms & Conditions
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Driver Download
-      </p>
-      <p className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]">
-        Do Not Sell My Personal Information
-      </p>
+      <div className="flex flex-col">
+        <Link
+          href="/contact-us"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Contact Us
+        </Link>
+        <Link
+          href="/about-us"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          About Us
+        </Link>
+        <Link
+          href="/shipping-policy"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Shipping Policy
+        </Link>
+        <Link
+          href="/refund-policy"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Refund Policy{" "}
+        </Link>
+        <Link
+          href="/privacy-policy"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Privacy Policy
+        </Link>
+        <Link
+          href="/terms-and-conditions"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Terms & Conditions
+        </Link>
+        <Link
+          href="/drivers-download"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Drivers Download
+        </Link>
+        <Link
+          href="/ccpa-opt-out"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          Do Not Sell My Personal Information
+        </Link>
+        <Link
+          href="/faq"
+          className="w-fit cursor-pointer duration-300 hover:text-[#EBEFF1]"
+        >
+          FAQ
+        </Link>
+      </div>
     </div>
   );
 }
