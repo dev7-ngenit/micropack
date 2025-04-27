7c8c15963e8561d084b8312591dd62abd858c9a3
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-27 11:17:44 +0600

    fix: fixed title size

--- src/app/(main)/(static)/ccpa-opt-out/page.js
+++ src/app/(main)/(static)/ccpa-opt-out/page.js
@@ -3,7 +3,7 @@ import Title from "@/components/shared/Title/Title";
 export default function page() {
   return (
     <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
-      <Title className="text-4xl [background-position-y:70%]">
+      <Title className="text-[1.4rem] [background-position-y:60%] md:text-4xl md:[background-position-y:70%]">
         Do not sell my personal information
       </Title>
 
