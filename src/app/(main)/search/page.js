4f8a72f645f6da716d83dc75a15ee29dcf3a158a
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-27 17:49:02 +0600

    add: created /search page and NoContent component

---
+++ src/app/(main)/search/page.js
@@ -0,0 +1,23 @@
+import Details from "@/components/search/Details/Details";
+import NoContent from "@/components/search/NoContent";
+import Title from "@/components/shared/Title/Title";
+
+export default async function page({ searchParams }) {
+  const { q } = (await searchParams) || {};
+
+  console.log(q);
+
+  return (
+    <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
+      {q && (
+        <Title className="text-4xl [background-position-y:70%]">
+          Search for “{q}”
+        </Title>
+      )}
+
+      <Details />
+
+      {!q && <NoContent />}
+    </main>
+  );
+}
---
+++ src/components/search/NoContent.jsx
@@ -0,0 +1,19 @@
+import { FaSearch } from "react-icons/fa";
+import { Card, CardContent, CardFooter } from "../ui/card";
+
+export default function NoContent({ props }) {
+  const { title = "Search", subtitle = "Use the search bar to find products" } =
+    props || {};
+  return (
+    <Card className="mx-auto mt-20 h-[20rem] max-w-[28rem] shadow-2xl">
+      <CardContent className="relative mx-auto mt-6 size-[10rem] rounded-full bg-[#3EAFE1] p-0 inset-shadow-[0_-20px_10px_#00000024]">
+        <FaSearch className="absolute top-2/3 left-[57%] -translate-x-1/2 -translate-y-1/2 text-8xl font-black text-white drop-shadow-[0_-15px_2px_rgba(0,_0,_0,_.08)]" />
+      </CardContent>
+
+      <CardFooter className="block text-center">
+        <h3 className="text-4xl font-bold">{title}</h3>
+        <p className="mt-2 text-xl text-gray-900/70">{subtitle}</p>
+      </CardFooter>
+    </Card>
+  );
+}
