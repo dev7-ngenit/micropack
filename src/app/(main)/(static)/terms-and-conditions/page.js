2904c1e54c719ec611f3075e9f436a44b9c5b882
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-04-27 10:49:58 +0600

    add: created mobile responsive /terms-and-conditions and /ccpa-opt-out pages

---
+++ src/app/(main)/(static)/terms-and-conditions/page.js
@@ -0,0 +1,26 @@
+import Title from "@/components/shared/Title/Title";
+
+export default function page() {
+  return (
+    <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
+      <Title className="text-4xl [background-position-y:70%]">
+        Terms and Conditions
+      </Title>
+
+      <section className="mt-10 flex flex-col gap-5 md:mt-16">
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Quisquam,
+      </section>
+    </main>
+  );
+}
