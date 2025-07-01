48c0f9cc7a2c8089300765930d08bde662dd8d24
dev7-ngenit <dev7.ngenit@gmail.com>
2025-07-01 15:40:56 +0600

    update: made the /faq page dynamic

--- src/components/faq/FAQs.jsx
+++ src/components/faq/FAQs.jsx
@@ -5,36 +5,38 @@ import {
   AccordionTrigger,
 } from "../ui/accordion";
 
-const faqs = [
-  {
-    question: "Are Micropack power products certified?",
-    answer:
-      "Yes, all of our products are certified and have the appropriate markings.",
-  },
-  {
-    question:
-      "Do power banks automatically turn on when they are plugged into a device?",
-    answer:
-      "Yes, most of our power banks will begin charging as soon as it is plugged into the device.",
-  },
-  {
-    question: "What does the power bank capacity mean?",
-    answer:
-      "Capacity is measured in milliamperes (mAh) on power banks. As a general rule, the more mAh in the power bank, the more times you will be able to charge your device before having to charge the power bank again.",
-  },
-];
-
-export default function FAQs() {
-  return (
-    <Accordion type="single" collapsible className="mt-5 w-full">
-      {faqs.map((faq, index) => (
-        <AccordionItem value={`item-${index + 1}`} key={index}>
-          <AccordionTrigger className="cursor-pointer text-lg">
-            {faq?.question}
-          </AccordionTrigger>
-          <AccordionContent className="text-lg">{faq?.answer}</AccordionContent>
-        </AccordionItem>
-      ))}
-    </Accordion>
+export default async function FAQs() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_ORIGIN}/v1/faq`,
+    {
+      next: {
+        revalidate: 60 * 60 * 24,
+      },
+    },
   );
+
+  const { data: faqs } = await response.json();
+
+  // Decide what to render
+  let content;
+  if (faqs?.length === 0) {
+    content = <p>No Content Found.</p>;
+  } else {
+    content = (
+      <Accordion type="single" collapsible className="mt-5 w-full">
+        {faqs.map((faq) => (
+          <AccordionItem value={`item-${faq?.id}`} key={faq?.id}>
+            <AccordionTrigger className="cursor-pointer text-lg">
+              {faq?.question}
+            </AccordionTrigger>
+            <AccordionContent className="text-lg">
+              {faq?.answer}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    );
+  }
+
+  return content;
 }
