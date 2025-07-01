19673ce536f60ef9437d5a9574e3eb70f229d263
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-07-01 13:16:02 +0600

    update: made the /terms-and-conditions page dynamic

--- src/app/(main)/(static)/terms-and-conditions/page.js
+++ src/app/(main)/(static)/terms-and-conditions/page.js
@@ -1,57 +1,18 @@
 import Title from "@/components/shared/Title/Title";
 
-export default function page() {
+export default async function page() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_ORIGIN}/v1/terms-and-condition`,
+  );
+  const { data } = await response.json();
+
   return (
     <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
       <Title className="text-4xl [background-position-y:70%]">
         Terms and Conditions
       </Title>
 
-      <section className="mt-10 flex flex-col gap-5 md:mt-16">
-        <div class="text-2xl font-semibold">Introductions</div>
-        <p>
-          These terms and conditions govern your use of this website, by using
-          this website, you accept these terms and conditions in full. If you
-          disagree with these terms and conditions or any part of these terms
-          and conditions, you must not use this website.
-        </p>
-        <p>
-          You must be at least 18 years of age to use this website. By using
-          this website and by agreeing to these terms and conditions you warrant
-          and represent that you are at least 18 years of age.
-        </p>
-
-        <h2 className="text-2xl font-semibold">License to Use Website</h2>
-        <p>
-          Unless otherwise stated, Micropack and/or its licensors own the
-          intellectual property rights in the website and material on the
-          website. Subject to the license below, all these intellectual property
-          rights are reserved.
-        </p>
-
-        <h2 className="text-2xl font-semibold">You must not:</h2>
-        <ul>
-          <li className="list-disc">
-            Republish material from this website (including republication on
-            another website)
-          </li>
-          <li className="list-disc">
-            Sell, rent or sub-license material from the website
-          </li>
-          <li className="list-disc">
-            Show any material from the website in public
-          </li>
-          <li className="list-disc">
-            Reproduce, duplicate, copy or otherwise exploit material on this
-            website for a commercial purpose.
-          </li>
-          <li className="list-disc">
-            Edit or otherwise modify any material on the website, or
-            redistribute material from this website (except for content
-            specifically and expressly made available for redistribution)
-          </li>
-        </ul>
-      </section>
+      <article className="prose mt-10 max-w-full">{data?.content}</article>
     </main>
   );
 }
