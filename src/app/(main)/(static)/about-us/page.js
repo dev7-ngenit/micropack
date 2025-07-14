3fd71e2848ebf50e0c411eb0120ce9e13b51747d
dev7-ngenit <dev7.ngenit@gmail.com>
2025-07-14 14:51:33 +0600

    update: made /about-us page dynamic

--- src/app/(main)/(static)/about-us/page.js
+++ src/app/(main)/(static)/about-us/page.js
@@ -1,32 +1,28 @@
 import Title from "@/components/shared/Title/Title";
+import { notFound } from "next/navigation";
 
-export default function page() {
+export default async function page() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_ORIGIN}/v1/about-us`,
+    {
+      cache: "no-store",
+    },
+  );
+
+  if (!res.ok) {
+    notFound();
+  }
+
+  const { data } = await res.json();
+
   return (
     <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
       <Title className="text-4xl [background-position-y:70%]">About Us</Title>
 
-      <section className="mt-10 flex flex-col gap-5 text-xl md:mt-16">
-        Improvement is an eternal theme nowadays, we always keep on improving.
-        Micropack evolves, makes progress, moves forward, and continuously
-        provides the best performance products for customers. Micropack is
-        registered in Hongkong, and the R&D center, product design department,
-        and production lines are located in guangzhou, China. In the early
-        stages, Micropack developed the mouse with the innovative feature of
-        Blue-Tech technology, which allowed the mouse to work on almost any
-        friconless desktop surface, including glass. After 8 years of efforts in
-        researing and developing in computer accessories, Micropack has expanded
-        its product range, including Mobile Accessories, Audio series and high
-        perfermance products. Micropack deeply focuses on high quality and
-        competitive consuming electronic field, our products are including
-        computer accessories, mobile accessories, audio series etc.. Micropack
-        understands the concepts of urban digital living and greatly focuses on
-        developing great performance products for its diversely connected
-        customers. We insure your digital life with safe, high quality and
-        stylisl exactly as our company motto says &quot; Digitally Yours&quot;
-        Micropack has sincere distribution partners globally, we are committed
-        to developing best products and offer sincere and high efficient service
-        to our loyal customers all over the world.
-      </section>
+      <article
+        className="prose mt-10 max-w-full"
+        dangerouslySetInnerHTML={{ __html: data?.content }}
+      ></article>
     </main>
   );
 }
