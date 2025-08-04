0efb2b109496213af90111ca1c12967909562f56
dev7-ngenit <dev7.ngenit@gmail.com>
2025-08-04 16:20:40 +0600

    update: added badge quantity icon to cart icon

--- src/components/shared/Navbar/NavIcons.jsx
+++ src/components/shared/Navbar/NavIcons.jsx
@@ -1,15 +1,25 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import useCart from "@/hooks/useCart";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import { FaBagShopping } from "react-icons/fa6";
 
 export default function NavIcons() {
+  const { cart } = useCart();
+
   return (
     <div className="flex items-center gap-5 text-2xl text-white md:order-last md:text-3xl">
       <Link href="/profile">
         <CgProfile className="hover:cursor-pointer" />
       </Link>
-      <Link href="/cart">
+
+      <Link href="/cart" className="relative w-fit">
         <FaBagShopping className="hover:cursor-pointer" />
+        <Badge className="absolute -top-2 -right-2 rounded-full bg-[#2F9ECF]">
+          {cart?.length}
+        </Badge>
       </Link>
     </div>
   );
