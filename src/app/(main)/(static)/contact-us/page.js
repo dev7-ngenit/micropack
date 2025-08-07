b9ace82d33437f4e4853a203a9745d80b847a71a
dev7-ngenit <dev7.ngenit@gmail.com>
2025-08-07 11:21:52 +0600

    update: redesigned contact us form and made it interactive

--- src/app/(main)/(static)/contact-us/page.js
+++ src/app/(main)/(static)/contact-us/page.js
@@ -1,24 +1,137 @@
-import Title from "@/components/shared/Title/Title";
+"use client";
 
-export default function page() {
+import Title from "@/components/shared/Title/Title";
+import { Button } from "@/components/ui/button";
+import useAxiosSecure from "@/hooks/useAxiosSecure";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export default function ContactPage() {
+  const axios = useAxiosSecure();
+  const [formDetails, setFormDetails] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormDetails({ ...formDetails, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("/v1/contact", formDetails);
+
+      setFormDetails({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+      });
+      toast.success("Message sent successfully!");
+    } catch (error) {
+      console.log("🚀 ~ handleSubmit ~ error:", error);
+      toast.error("Something went wrong! Please try again.");
+    }
+  };
+
   return (
     <main className="container !mt-10 box-border px-5 md:!my-16 md:box-content md:min-h-[calc(100dvh-33rem)]">
-      <Title className="text-4xl [background-position-y:70%]">Contact Us</Title>
+      <div className="mx-auto max-w-screen-md px-4 py-8 lg:py-16">
+        <Title className="mx-auto mb-2 text-4xl [background-position-y:70%]">
+          Contact Us
+        </Title>
 
-      <section className="mt-10 flex flex-col gap-5 md:mt-16">
-        <h3 className="text-xl">
-          If you have any questions or concerns regarding our products or
-          services, please feel free to contact us via email or live chat on our
-          website
-        </h3>
+        <p className="mb-8 text-center font-light text-gray-500 sm:text-xl lg:mb-16 dark:text-gray-400">
+          Got a technical issue? Want to send feedback about us? Need details
+          about our Business plan? Let us know.
+        </p>
 
-        <h3 className="text-xl">Email：<span className="underline">micropack@micropackhk.com</span></h3>
-
-        <h3 className="text-xl">
-          We strive to provide excellent customer service and will do our best
-          to respond to your inquiries as quickly as possible.
-        </h3>
-      </section>
+        <form className="space-y-8" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="name"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              name="name"
+              placeholder="John Doe"
+              value={formDetails.name}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              name="email"
+              placeholder="name@mail.com"
+              value={formDetails.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="subject"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              Subject
+            </label>
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              placeholder="Let us know how we can help you"
+              value={formDetails.subject}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="sm:col-span-2">
+            <label
+              htmlFor="message"
+              className="mb-2 block text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              Your message
+            </label>
+            <textarea
+              id="message"
+              rows="6"
+              name="message"
+              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              placeholder="Leave a comment..."
+              value={formDetails.message}
+              onChange={handleInputChange}
+              required
+            ></textarea>
+          </div>
+          <Button
+            type="submit"
+            className="focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mx-auto block cursor-pointer rounded-lg bg-[#2F9ECF] px-5 py-3 text-center text-sm font-medium text-white hover:bg-[#2F9ECF] focus:ring-4 focus:outline-none sm:w-fit"
+          >
+            Send message
+          </Button>
+        </form>
+      </div>
     </main>
   );
 }
